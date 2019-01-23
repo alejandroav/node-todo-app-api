319ec204ee933eb4af968aca3d50e6f403dd9cf2
Alejandro Alarcón Villena <alejandroalarcon93@gmail.com>
2019-01-23 18:39:39 +0100

    delete tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -33,7 +33,7 @@ describe('POST /todos', () => {
 			.end((err, res) => {
 				if (err) {
 					return done(err);
-				};
+				}
 
 				Todo.find({text}).then((todos) => {
 					expect(todos.length).toBe(1);
@@ -51,7 +51,7 @@ describe('POST /todos', () => {
 			.end((err, res) => {
 				if (err) {
 					return done(err);
-				};
+				}
 
 				Todo.find().then((todos) => {
 					expect(todos.length).toBe(2);
@@ -84,7 +84,7 @@ describe('GET /todos/:id', () => {
 			.end(done);
 	});
 
-	it('should return a 404 if todo not found', (done) => {
+	it('should return a 404 if not found', (done) => {
 		request(app)
 			.get(`/todos/${new ObjectID().toHexString()}`)
 			.expect(404)
@@ -94,6 +94,42 @@ describe('GET /todos/:id', () => {
 	it('should return a 404 if invalid id', (done) => {
 		request(app)
 		.get('/todos/123')
+		.expect(404)
+		.end(done);
+	});
+});
+
+describe('DELETE /todos/:id', () => {
+	it('should remove a todo', (done) => {
+		var id = todos[1]._id.toHexString();
+		request(app)
+			.delete(`/todos/${id}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo._id).toBe(id);
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				} else {
+					Todo.findById(id).then((todo) => {
+						expect(todo).toNotExist();
+						done();
+					}).catch((err) => done(err));
+				}
+			});
+	});
+
+	it('should return a 404 if not found', (done) => {
+		request(app)
+			.delete(`/todos/${new ObjectID().toHexString()}`)
+			.expect(404)
+			.end(done);
+	});
+
+	it('should return 404 if invalid id', (done) => {
+		request(app)
+		.delete('/todos/123')
 		.expect(404)
 		.end(done);
 	});
