56894566415d353ba1ebf3130755f209e7d7b1b9
Alejandro Alarcón Villena <alejandroalarcon93@gmail.com>
2019-01-28 17:17:49 +0100

    POST /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -80,6 +80,25 @@ UserSchema.statics.findByToken = function(token) {
 	});
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+	return User.findOne({email})
+		.then((user) => {
+			if (!user) {
+				return Promise.reject('User not found.');
+			} else {
+				return new Promise((resolve, reject) => {
+					bcrypt.compare(password, user.password, (err, res) => {
+						if (!res) {
+							reject(err);
+						} else {
+							resolve(user);
+						}
+					});
+				})
+			}
+		});
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
